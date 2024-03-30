a7b928f2f9d9bfed3b069d2cdac5a378d1515b2c
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-03-30 14:18:58 +0100

    Added test structure

--- src/layoutimg/__init__.py
+++ src/layoutimg/__init__.py
@@ -1,8 +1,8 @@
 
 from importlib import resources
 import tomli
+from . import arith
 
 __version__ = "1.0.0"
 
-config = tomli.loads(resources.read_text("layoutimg", "config.toml"))
-DISPLAY_TEXT = config["main"]["display_text"]
+CONFIG = tomli.loads(resources.read_text("layoutimg", "config.toml"))
---
+++ src/layoutimg/arith.py
@@ -0,0 +1,3 @@
+
+def sq_sum(nums: 'list[float]') -> float:
+    return sum([n * n for n in nums])
