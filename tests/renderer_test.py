b4e6f2c91de7881dd5e1cfa5ea9414a26a50f6ec
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-03-30 15:35:12 +0100

    Added more renderer tests

--- tests/renderer_test.py
+++ tests/renderer_test.py
@@ -13,4 +13,21 @@ def test_multiple_rect():
 def test_render_text():
     r = ImageRenderer()
     r.draw_text(0, 0, "Hello World!")
-    r.save("test.png")
+
+def test_no_expand():
+    r = ImageRenderer(10, 10, expand=False)
+    r.draw_rect(50, 50, 100, 100)
+    assert r.width == 10 and r.height == 10
+
+def test_disable_expand():
+    r = ImageRenderer()
+    r.draw_rect(50, 50, 100, 100)
+    w, h = r.width, r.height
+    r.expand = False
+    r.draw_rect(100, 100, 100, 100)
+    assert r.width == w and r.height == h
+
+def test_very_long_text():
+    r = ImageRenderer()
+    text = "Lorem Ipsum " * 100
+    r.draw_text(0, 0, text)
