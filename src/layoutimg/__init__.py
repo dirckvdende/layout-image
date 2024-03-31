b6861892a041b0dc242f849e706168ff37297186
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-03-31 18:08:50 +0200

    Small name changes

--- src/layoutimg/__init__.py
+++ src/layoutimg/__init__.py
@@ -1,8 +1,9 @@
 
-from importlib import resources
-import tomli
+from importlib import resources as _resources
+import tomli as _tomli
+
 from .layoutimg import LayoutImage
 
 __version__ = "1.0.0"
 
-CONFIG = tomli.loads(resources.read_text("layoutimg", "config.toml"))
+CONFIG = _tomli.loads(_resources.read_text("layoutimg", "config.toml"))
