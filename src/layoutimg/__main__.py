54eb6fed6e0385a4921991a38fd70fcdc50b81f1
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-03-31 18:46:37 +0200

    Replaced main script functionality

--- src/layoutimg/__main__.py
+++ src/layoutimg/__main__.py
@@ -1,8 +1,20 @@
 
-from layoutimg import DISPLAY_TEXT
+import sys
+from .layoutimg import LayoutImage
 
 def main():
-    print(DISPLAY_TEXT)
+    """ Generate one of the examples using the main function """
+    if len(sys.argv) <= 1:
+        print("Generate a layout image given by its path name. The output fill "
+        "have the same name as the input file, but with a PNG extension.")
+        return
+    input_filename = sys.argv[1]
+    output_filename = input_filename + ".png"
+    with open(input_filename, "r") as input_file:
+        text = input_file.read()
+    image = LayoutImage(text)
+    image.generate()
+    image.save(output_filename)
 
 if __name__ == "__main__":
     main()
