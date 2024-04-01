c6a2bd7aa0e60355fc13c08d1631990fba80b9e7
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-04-01 13:10:14 +0200

    Added subdirectory search to examples test (#4)

--- tests/example_test.py
+++ tests/example_test.py
@@ -4,13 +4,12 @@ import os
 import pathlib
 from layoutimg import LayoutImage
 
-# Find all XML files in the examples directory
-files = [f for f in os.listdir("examples/") if pathlib.Path(f).suffix == ".xml"]
+# Find all XML files in the examples directory and subdirectories
+files = list(pathlib.Path("examples/").rglob("*.xml"))
 
-@pytest.mark.parametrize("name", files)
-def test_example(name: str):
+@pytest.mark.parametrize("filename", files)
+def test_example(filename: str):
     """ Test converting one of the examples to an image """
-    filename = f"examples/{name}"
     with open(filename, "r") as f:
         text = f.read()
     image = LayoutImage(text)
