8666d8160a2c38f953dc4d202126ffe1eeb12e81
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-04-02 10:42:18 +0200

    Fixed bug in renderer with off-by-one error (#13)

--- src/layoutimg/renderer/renderer.py
+++ src/layoutimg/renderer/renderer.py
@@ -29,7 +29,7 @@ class ImageRenderer:
         if dx < 1 or dy < 1:
             return
         self.expand_image(x, y, dx, dy)
-        self._draw.rectangle((x, y, x + dx - 1, y + dy - 1), fill=color)
+        self._draw.rectangle((x, y, x + dx, y + dy), fill=color)
 
     def draw_text(self, x: int, y: int, text: str, *, font: 'str | None' = None,
     color: str = "black", font_size: int = 64, only_bbox: bool = False):
@@ -56,7 +56,8 @@ class ImageRenderer:
             return
         self.expand_image(x, y, dx, dy)
         with Image.open(path) as image_file:
-            self._image.paste(image_file, (x, y, x + dx - 1, y + dy - 1))
+            resized_image = image_file.resize((dx, dy))
+            self._image.paste(resized_image, (x, y, x + dx, y + dy))
 
     @property
     def image(self):
@@ -91,7 +92,7 @@ class ImageRenderer:
     def _bbox_convert(self, bbox: 'tuple[int, int, int, int]'):
         """ Convert a PIL bounding box to a bounding box of the form
             (x, y, dx, dy) """
-        return bbox[0], bbox[1], bbox[2] - bbox[0] + 1, bbox[3] - bbox[1] + 1
+        return bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]
 
     def expand_image(self, x: int, y: int, dx: int, dy: int):
         """ Expand the image, given some bounding box that should be included in
